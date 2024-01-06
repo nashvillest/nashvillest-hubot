58795a3f4c78f1b2d4b8689f9fa11755fc3f5c9c
Stephen Yeargin <stephen.yeargin@gmail.com>
2024-01-06 11:47:06 -0600

    Slack formatting

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,25 +12,88 @@
 //   HUBOT_JENKINS_JOB - Job ID for Hubot deployment
 
 module.exports = (robot) => {
-  robot.respond(/deploy\s?(.*)/i, (msg) => {
-    msg.send('Initiating deployment ...');
+  const handleError = ({
+    err, res, body, msg, threadId,
+  }) => {
+    robot.logger.error(err || res.statusMessage);
+    robot.logger.error(body);
+    msg.send({
+      username: 'jenkins',
+      icon_emoji: ':cry:',
+      attachments: [{
+        text: `**Error deploying:** ${err?.message || res.statusMessage}`,
+        color: 'danger',
+        mrkdwn_in: ['text'],
+      }],
+      thread_ts: threadId,
+    });
+  };
+
+  robot.respond(/(?:bot\s)?deploy\s?(.*)/i, (msg) => {
     const auth = Buffer.from(`${process.env.HUBOT_JENKINS_USER}:${process.env.HUBOT_JENKINS_AUTH_TOKEN}`).toString('base64');
+    const headers = {
+      Accept: 'application/json',
+      Authorization: `Basic ${auth}`,
+    };
+    const branch = msg.match[1] || 'main';
+    const threadId = msg.message.rawMessage.ts;
+    robot.logger.debug({ branch, threadId });
+    msg.send({
+      username: 'Jenkins',
+      icon_emoji: ':shipit:',
+      attachments: [{
+        text: `Starting deploy of \`${branch}\` ...`,
+        mrkdwn_in: ['text'],
+        color: 'blue',
+      }],
+    });
     robot.http(`${process.env.HUBOT_JENKINS_URL}/job/${process.env.HUBOT_JENKINS_JOB}/buildWithParameters`)
-      .headers({
-        Accept: 'application/json',
-        Authorization: `Basic ${auth}`,
-      })
+      .headers(headers)
       .post({
-        BRANCH: msg.match[1] || 'main',
+        BRANCH: branch,
       })((err, res, body) => {
         robot.logger.debug(res);
         robot.logger.debug(body);
         if (err || res.statusCode !== 201) {
-          robot.logger.error(err || res.statusMessage);
-          msg.send(err?.message || res.statusMessage);
+          handleError({
+            err, res, body, msg, threadId,
+          });
           return;
         }
-        msg.send('Deployment initiated.');
+        msg.send({
+          username: 'Jenkins',
+          icon_emoji: ':hourglass:',
+          attachments: [{
+            text: `Deploy of \`${branch}\` started. Getting status ...`,
+            mrkdwn_in: ['text'],
+            color: 'success',
+          }],
+          thread_ts: threadId,
+        });
+        robot.http(`${process.env.HUBOT_JENKINS_URL}/job/${process.env.HUBOT_JENKINS_JOB}/api/json`)
+          .headers(headers)
+          .get()((err2, res2, body2) => {
+            robot.logger.debug(res2);
+            robot.logger.debug(body2);
+            if (err2 || res2.statusCode !== 200) {
+              handleError({
+                err2, res2, body2, msg, threadId,
+              });
+              return;
+            }
+            const data = JSON.parse(body2);
+            msg.send({
+              username: 'Jenkins',
+              icon_emoji: ':white_checkmark:',
+              attachments: [{
+                title: data.fullDisplayName,
+                title_link: data.url,
+                text: 'Added to build queue. Click the link above to monitor.',
+                color: 'success',
+              }],
+              thread_ts: threadId,
+            });
+          });
       });
   });
 };
